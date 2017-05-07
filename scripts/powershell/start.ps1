ab51a5bae6364a1f202a0de41ce3eafa2c4a512a
StefanJanssen95 <stefanjanssen@outlook.com>
2017-05-07 11:56:16 +0200

    Added docker containers and powershell scripts

---
+++ scripts/powershell/start.ps1
@@ -0,0 +1,13 @@
+﻿Set-Location $PSScriptRoot
+
+[xml]$Config = Get-Content "config.xml"
+Set-Location ($Config.Settings.DockerComposeLocation)
+
+$Containers = $Config.Settings.Containers.Container | ForEach-Object {
+    $_
+}
+# Write-Output "Starting containers";
+docker-compose up -d $Containers
+
+Set-Location $PSScriptRoot
+Set-Location $Config.Settings.AfterDoneLocation
