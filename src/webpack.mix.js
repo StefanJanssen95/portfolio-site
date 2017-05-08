b2d9f3ebf6112a113b98b465dd27fc0f130c8754
StefanJanssen95 <stefanjanssen@outlook.com>
2017-05-08 19:44:58 +0200

    working laravel mix

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -15,8 +15,7 @@ mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .options({
         postCss: [
-            require('postcss-cssnext'),
-
+            require('postcss-cssnext')
         ]
     })
     .sourceMaps();;
